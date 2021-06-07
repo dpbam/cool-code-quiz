a4f107acceee327ff71a8d6ccdbbf020292fe737
Derek Phelps <derekphtpt@gmail.com>
2021-06-06 20:48:29 -0600

    high scores with initials unordered

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,9 +1,33 @@
 var finalScore = document.getElementById("finalScore");
 
 var endScore = localStorage.getItem("mostRecentScore");
+var saveScoreBtn = document.getElementById("saveScoreBtn");
+var initials = document.getElementById("username");
+var topScores = "";
 
-console.log(endScore);
+endScore = parseInt(endScore) + 1;
 
-endScore = parseInt(endScore);
+finalScore.innerText = endScore;
 
-finalScore.innerText = endScore + 1;
+// onclick = saveHighScore(event);
+
+saveScoreBtn.addEventListener("click", function () {
+  var scoreString = localStorage.getItem("score");
+  console.log("scoreString" + scoreString);
+
+  var scores = JSON.parse(scoreString);
+  console.log(scores);
+
+  if (scores == null) {
+    scores = {};
+  }
+
+  scores[initials.value] = endScore;
+
+  scoreString = JSON.stringify(scores);
+  console.log(scoreString);
+
+  localStorage.setItem("score", scoreString);
+  console.log(initials.value);
+});
+// localStorage.setItem("mostRecentScore", timeLeft);
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,4 +2,22 @@
 // rank them highest to lowest
 // only list 3 of the highest scores
 
+var highNumbers = document.getElementById("highscores-numbers");
 var highestScores = document.getElementsByClassName("highscores-box");
+var highScores = document.getElementsByClassName("highscores-numbers");
+
+var highScores = localStorage.getItem("score");
+console.log(highScores);
+
+var higherScores = JSON.parse(highScores);
+
+// highNumbers.innerText = higherScores;
+
+const entries = Object.entries(higherScores);
+// console.log(entries);
+
+// console.log(entries[1][0]);
+for (var i = 0; i < entries.length; i++) {
+  console.log(entries[i]);
+  highNumbers.innerHTML += entries[i][0] + "  " + entries[i][1] + "<br />";
+}
