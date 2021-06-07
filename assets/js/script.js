fd2ad72466d38ab2463e2e0ed594a436c28379be
Derek Phelps <derekphtpt@gmail.com>
2021-06-06 23:01:01 -0600

    terrible css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,11 @@
-// The array of questions for the game.
-// var questions = [
-//     { q: 'JavaScript question #1?', a: ['answer #1', 'answer #2', 'answer #3'], correct: 'answer #2' },
-//     { q: 'JavaScript question #2?', a: ['answer #1', 'answer #2', 'answer #3'], correct: 'answer #1' },
-//     { q: 'JavaScript question #3?', a: ['answer #1', 'answer #2', 'answer #3'], correct: 'answer #3' },
-//     { q: 'JavaScript question #4?', a: ['answer #1', 'answer #2', 'answer #3'], correct: 'answer #3' },
-//     { q: 'JavaScript question #5?', a: ['answer #1', 'answer #2', 'answer #3'], correct: 'answer #1' },
-
-// ];
-
 const question = document.querySelector("#question");
 const choices = Array.from(document.querySelectorAll(".choice-text"));
-// const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector("#score");
 const game = document.querySelector("#game");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
 let questionCounter = 0;
-// let availableQuestions = []
 
 // Start the timer at:
 var timeLeft = 100;
@@ -38,8 +26,6 @@ function countdown() {
     } else {
       timerEl.textContent = "";
       clearInterval(timeInterval);
-      // displayMessage("the game is over. please enter your initials");
-      // window.prompt("the game is over. please enter your initials")
     }
   }, 1000);
 }
@@ -87,38 +73,11 @@ let questions = [
   },
 ];
 
-// // loop over every question object
-// for (var i = 0; i < questions.length; i++) {
-//     // Display current question to user and ask OK/Cancel
-//     var answer = confirm(questions[i].q);
-
-//     // compare answers
-//     if (
-//         (questions[i].a === questions[i].correct)
-//     ) {
-//         // increase score
-//         score++;
-//         // alert the user
-//         // alert('Correct!');
-//     } else {
-//         // alert('Wrong!');
-//     };
-// };
-
-// // Show total at end
-// alert('You got ' + score + '/' + questions.length);
-
-// if(score > highScores) then score = highScores?
-
-// var score = (timeLeft);
-
 // index is total number of questions minus one
 const maxQuestions = 4;
 
 startGame = function () {
   questionCounter = 0;
-  // score = 0
-  // availableQuestions = questions
   getNewQuestion();
 };
 
@@ -128,9 +87,6 @@ getNewQuestion = function () {
     return window.location.assign("./end.html");
   }
 
-  // progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-  // progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
   currentQuestion = questions[questionCounter];
   question.innerText = currentQuestion.question;
   questionCounter++;
@@ -139,13 +95,6 @@ getNewQuestion = function () {
     var choice = choices[i];
     choice.innerText = currentQuestion["choice" + (i + 1)];
   }
-
-  // choices.forEach(choice = function() {
-  //     const number = choice.dataset['choicenumber']
-  //     choice.innerText = currentQuestion['choice' + number]
-  // })
-
-  // availableQuestions.splice(questionsIndex, 1)
 
   acceptingAnswers = true;
 };
@@ -174,32 +123,6 @@ for (var i = 0; i <= choices.length - 1; i++) {
     }, 500);
   });
 }
-
-// choices.forEach((choice) => {
-//   choice.addEventListener("click", (e) => {
-//     if (!acceptingAnswers) return;
-
-//     acceptingAnswers = false;
-//     const selectedChoice = e.target;
-//     const selectedAnswer = selectedChoice.dataset["number"];
-
-//     let classToApply =
-//       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-//     if (classToApply === "correct") {
-//       // go to next question
-//     }
-
-//     selectedChoice.parentElement.classList.add(classToApply);
-
-//     setTimeout(function () {
-//       selectedChoice.parentElement.classList.remove(classToApply);
-//       getNewQuestion();
-//     }, 1000);
-//   });
-// });
-
-// if get question wrong, then timeLeft - 10
 
 startBtn.onclick = countdown;
 startGame();
